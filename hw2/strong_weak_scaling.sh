a63465953ebe00bf9544af7e7a396d2299e0dc0a
Molly Zhang <mollyzhang@ucsc.edu>
2016-05-14 18:00:57 -0700

    reduce the number of processors to 4

--- hw2/strong_weak_scaling.sh
+++ hw2/strong_weak_scaling.sh
@@ -15,26 +15,3 @@ export OMP_NUM_THREADS=4
 echo number of threads $OMP_NUM_THREADS
 ./calculate_pi_openMP.x
 
-export OMP_NUM_THREADS=5
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
-
-export OMP_NUM_THREADS=6
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
-
-export OMP_NUM_THREADS=7
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
-
-export OMP_NUM_THREADS=8
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
-
-export OMP_NUM_THREADS=9
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
-
-export OMP_NUM_THREADS=10
-echo number of threads $OMP_NUM_THREADS
-./calculate_pi_openMP.x
